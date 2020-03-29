8392cf7f8990e7c4f5e91cf187511549e7192b16
wolfp <peter.wolf1981@web.de>
2020-03-29 19:44:12 +0200

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("ProjectSnailMail")
+    return True
 
 if __name__ == "__main__":
     main()
--- tst_first.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import main
-
-class MyTestCase(unittest.TestCase):
-    def tst_main(self):
-        self.assertEqual(main.main(), True)
-
-if __name__ == '__main__':
-    unittest.main()
